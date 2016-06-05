61415f44f654709181cfc97708e20a17aaf8922e
sandybuana7688 <sandy7688@students.amikom.ac.id>
2016-06-05 09:09:23 +0700

    pwsport

---
+++ konten/terbaru.php
@@ -0,0 +1,50 @@
+<?php sleep(3); ?>
+						<div class="blog-grids">
+							<div class="blog-grid-left">
+								<a href="rossi.html"><img src="images/rossi.jpg" class="img-responsive" alt=""/></a>
+							</div>
+							<div class="blog-grid-right">
+								
+								<h5><a href="rossi.html">Peran Teknologi sangat Membantu Aktivitas Valentino Rossi</a> </h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="blog-grids">
+							<div class="blog-grid-left">
+								<a href="berita_rio.html"><img src="images/rio.jpg" class="img-responsive" alt=""/></a>
+							</div>
+							<div class="blog-grid-right">
+								
+								<h5><a href="berita_rio.html">Rio Haryanto Menghitung Hari di Sirkus Formula 1 Musim 2016</a> </h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="blog-grids">
+							<div class="blog-grid-left">
+								<a href="berita_prancis.html"><img src="images/pogba.jpg" class="img-responsive" alt=""/></a>
+							</div>
+							<div class="blog-grid-right">
+								
+								<h5><a href="berita_prancis.html">Laga Pemanasan, Prancis Susah Payah Kalahkan Kamerun</a> </h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="blog-grids">
+							<div class="blog-grid-left">
+								<a href="berita_rashford.html""><img src="images/rashford.jpg" class="img-responsive" alt=""/></a>
+							</div>
+							<div class="blog-grid-right">
+								<h5><a href="berita_rashford.html">MU Pagari Rasford 4 Tahun</a> </h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="blog-grids">
+							<div class="blog-grid-left">
+								<a href="berita_denmark.html"><img src="images/mogensen.jpg" class="img-responsive" alt=""/></a>
+							</div>
+							<div class="blog-grid-right">
+								
+								<h5><a href="berita_denmark.html">Spesialis Ganda Asal Denmark Carsten Mogensen Kembali ke Bulu Tangkis</a> </h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
